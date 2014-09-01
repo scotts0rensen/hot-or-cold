f31bd9ae408aa71131cb609de1b33b857067a32b
Scott Sorensen <scott.sorensen@octanner.com>
2014-09-01 15:57:11 -0600

    add new game and form submit handlers

--- js/app.js
+++ js/app.js
@@ -1,17 +1,31 @@
 
 $(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
 
-  	});
+  /*--- Display information modal box ---*/
+  $(".what").click(function(){
+    $(".overlay").fadeIn(1000);
+  });
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+  /*--- Hide information modal box ---*/
+  $("a.close").click(function(){
+    $(".overlay").fadeOut(1000);
+  });
 
+  /*--- New game ---*/
+  $(".new").click( newGame );
+
+  /*--- Handle guesses ---*/
+  $( "form" ).submit( guessHandler );
+
+  /*--- Start new game on page load ---*/
+  newGame();
 });
 
+function guessHandler(event) {
+  alert( "Handler for .submit() called." );
+  event.preventDefault();
+}
 
+function newGame() {
+  alert( "Starting new game" );
+}
